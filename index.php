fcf688e334426c7d09d30d06ecf6f5f4c9bc8c39
Samson <samson.c.chung@gmail.com>
2016-09-22 23:07:05 -0400

    Add styles and drop down

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8"> 
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- Custom Stylesheet -->
+<link rel="stylesheet" type="text/css" href="style-main.css" />
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+
+<body>
+
+<div class="container">
+	<div class="row">
+
+	<h1>Samson Chung</h1>
+
+	<div class="col-md-8">
+	
+		<div class="panel panel-default">
+
+			<div class="panel-body panel-body-custom">
+				<span class="glyphicon glyphicon-education"></span>
+				<a data-toggle="collapse" href="#collapse1">4th year Computer Science Student @ UofT (St. George)</a>
+			</div>
+			<div id="collapse1" class="panel-collapse collapse">
+				<div class="panel-collapse collapse panel-body-custom">Computer Science blah</div>
+			</div>
+
+			<div class="panel-body panel-body-custom">
+				<span class="glyphicon glyphicon-briefcase"></span>
+				<a data-toggle="collapse" href="#collapse2">Former Intern at the 407ETR (IT)</a>
+				</div>
+			<div id="collapse2" class="panel-collapse collapse">
+				<div class="panel-collapse collapse panel-body-custom">Computer Science blah</div>
+			</div>
+
+			<div class="panel-body panel-body-custom">
+				<img id="dumbbell" src="dumbbell.png" width="24px" height="24px">
+				<a data-toggle="collapse" href="#collapse3">Fitness and Exercise Enthusiast
+				</div>
+			<div id="collapse3" class="panel-collapse collapse">
+				<div class="panel-collapse collapse panel-body-custom">Computer Science blah</div>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div class="col-md-4">
+		<div class="panel panel-default">
+			<div class="panel-body panel-body-custom">samson.c.chung@gmail.com</div>
+		</div>
+	</div>
+
+	</div>
+</div>
+
+</body>
+
+</html>
