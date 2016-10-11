a1f98752c097a2dbca1098ea6f5999592f40ea78
Samson <samson.c.chung@gmail.com>
2016-10-11 17:47:20 -0400

    update style and descriptions

--- index.php
+++ index.php
@@ -24,46 +24,79 @@
 
 <body>
 
+<nav id="navbar" class="navbar navbar-static-top">
+
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Samson Chung</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Resume</a></li>
+    </ul>
+  </div>
+
+</nav>
+
+
 <div class="container">
 	<div class="row">
 
-	<h1>Samson Chung</h1>
-
 	<div class="col-md-8">
-	
-		<div class="panel panel-default">
+		<div class="panel-group" id="accordion">
+			<div class="panel panel-default">
 
-			<div class="panel-body panel-body-custom">
-				<span class="glyphicon glyphicon-education"></span>
-				<a data-toggle="collapse" href="#collapse1">4th year Computer Science Student @ UofT (St. George)</a>
-			</div>
-			<div id="collapse1" class="panel-collapse collapse">
-				<div class="panel-collapse collapse panel-body-custom">Computer Science blah</div>
-			</div>
-
-			<div class="panel-body panel-body-custom">
-				<span class="glyphicon glyphicon-briefcase"></span>
-				<a data-toggle="collapse" href="#collapse2">Former Intern at the 407ETR (IT)</a>
+				<a data-toggle="collapse" href="#collapse1" data-parent="#accordion">
+					<div class="panel-body panel-body-custom">
+						<span class="glyphicon glyphicon-education"></span>
+						4th year Computer Science Student @ UofT (St. George)
+					</div>
+				</a>
+				<div id="collapse1" class="panel-collapse collapse">
+						<div class="panel-collapse collapse panel-body-custom">
+							Computer Science Specialist<br>
+							Interest in various fields of computer science, including software engineering, web technologies, and artifical intelligence
+						</div>
 				</div>
-			<div id="collapse2" class="panel-collapse collapse">
-				<div class="panel-collapse collapse panel-body-custom">blah blah</div>
-			</div>
 
-			<div class="panel-body panel-body-custom">
-				<img id="dumbbell" src="dumbbell.png" width="24px" height="24px">
-				<a data-toggle="collapse" href="#collapse3">Fitness and Exercise Enthusiast</a>
+				<a data-toggle="collapse" href="#collapse2" data-parent="#accordion">
+					<div class="panel-body panel-body-custom">
+						<span class="glyphicon glyphicon-briefcase"></span>
+						DevOps + SysAdmin Internship @ <a class="exLink" href=https://407etr.com>407ETR Concessions Company</a>
+						</div>
+				</a>
+				<div id="collapse2" class="panel-collapse collapse">
+					<div class="panel-collapse collapse panel-body-custom">
+						Customizing and maintaining DevOps stack<br>
+						<div style="text-indent: 3em; color: grey">Rundeck, Jenkins, Nexus, and Sonar</div>
+						Maintaining and monitoring of RHEL servers and other Linux servers<br>
+						<div style="text-indent: 3em; color: grey">Nagios/Thruk, VMWare</div>
+					</div>
 				</div>
-			<div id="collapse3" class="panel-collapse collapse">
-				<div class="panel-collapse collapse panel-body-custom">blahblahblah</div>
-			</div>
+				
 
+				<a data-toggle="collapse" href="#collapse3" data-parent="#accordion">
+					<div class="panel-body panel-body-custom">
+						<img id="dumbbell" src="dumbbell.png" width="24px" height="24px">
+						Fitness and Exercise Enthusiast
+						</div>
+				</a>
+				<div id="collapse3" class="panel-collapse collapse">
+					<div class="panel-collapse collapse panel-body-custom">blahblahblah</div>
+				</div>
+
+
+			</div>
 		</div>
-
 	</div>
 
 	<div class="col-md-4">
 		<div class="panel panel-default">
-			<div class="panel-body panel-body-custom">samson.c.chung@gmail.com</div>
+			<div class="panel-body panel-body-custom">
+				<span class="glyphicon glyphicon-envelope"></span>
+				samson.c.chung@gmail.com
+			</div>
 		</div>
 	</div>
 
